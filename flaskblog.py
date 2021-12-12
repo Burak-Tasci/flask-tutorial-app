5910d93348824ade6347202757187a8084f87f1f
Burak Tasci <buraaktascii@outlook.com>
2021-12-12 21:45:22 +0300

    Updated

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+
+	{
+		'author': 'Corey Schafer',
+		'title': 'Blog Post 1',
+		'content': 'First post content',
+		'date_posted':'April 20, 2018'
+	},
+	{
+		'author': 'John Doe',
+		'title': 'Blog Post 2',
+		'content': 'Second post content',
+		'date_posted':'April 25, 2018'
+	}
+]
 
 
 @app.route("/")
-def hello():
-	return "<h1>Hello World</h1>"
+@app.route("/home")
+def HomePage():
+	return render_template("home.html", posts=posts)
+
+@app.route("/about")
+def AboutPage():
+	return render_template("about.html", title="About")
